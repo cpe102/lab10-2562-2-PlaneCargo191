017da15399b8083bbbea020349011606744670a9
PlaneCargo191 <t.james_17@hotmail.com>
2020-01-31 17:17:02 +0700

    New

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,32 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    string x;
+
+    cout << "Press Enter 3 times to reveal your future.";
+    getline(cin,x);
+    getline(cin,x);
+
+    cin.get();
+    srand(time(0));
+    int g = rand()%9;
+
+    if(g==0) x="A";
+    else if(g==1) x="B+";
+    else if(g==2) x="B";
+    else if(g==3) x="C+";
+    else if(g==4) x="C";
+    else if(g==5) x="D+";
+    else if(g==6) x="D";
+    else if(g==7) x="F";
+    else x="W";
+
+    cout << "You will get " << x << " in this 206102.";
+
+    return 0;
+}
